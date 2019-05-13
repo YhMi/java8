e0b9a40b7905fb75162dc2d826d620e58df321bd
yhmi <13181657631@163.com>
2019-05-13 16:32:14 +0800

    初始化

--- src/main/java/top/yhmi/StreamReduce.java
+++ src/main/java/top/yhmi/StreamReduce.java
@@ -1,7 +1,14 @@
 package top.yhmi;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StreamReduce {
     public static void main(String[] args) {
-
+        Stream<Integer> stream = Arrays.stream(new Integer[]{1, 2, 3, 4, 5, 6, 7});
+        Integer reduce = stream.reduce(0, (i, j) -> i + j);
+        System.out.println(reduce);
+        stream = Arrays.stream(new Integer[]{1, 2, 3, 4, 5, 6, 7});
+        stream.reduce((i,j)->i+j).ifPresent(System.out::println);
     }
 }
